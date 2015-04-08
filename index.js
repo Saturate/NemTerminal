30a0aaf373e8ac7d6535a4567548066941fd24c7
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-04-08 20:46:44 +0200

    Add PIN step and more logic

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 // https://www.portalprotect.dk/demologinnemidjs.jsp
+
+(function ($) {
+
+/**
+* @function
+* @property {object} jQuery plugin which runs handler function once specified element is inserted into the DOM
+* @param {function} handler A function to execute at the time when the element is inserted
+* @param {bool} shouldRunHandlerOnce Optional: if true, handler is unbound after its first invocation
+* @example $(selector).waitUntilExists(function);
+*/
+
+$.fn.waitUntilExists = function (handler, shouldRunHandlerOnce, isChild) {
+    var found       = 'found';
+    var $this       = $(this.selector);
+    var $elements   = $this.not(function () { return $(this).data(found); }).each(handler).data(found, true);
+
+    if (!isChild)
+    {
+        (window.waitUntilExists_Intervals = window.waitUntilExists_Intervals || {})[this.selector] =
+            window.setInterval(function () { $this.waitUntilExists(handler, shouldRunHandlerOnce, true); }, 500)
+        ;
+    }
+    else if (shouldRunHandlerOnce && $elements.length)
+    {
+        window.clearInterval(window.waitUntilExists_Intervals[this.selector]);
+    }
+
+    return $this;
+}
+
+}(jQuery));
+
+
 jQuery(document).ready(function($) {
+    var events;
+    // Prepend the terminal DOM nodes
     $('body').prepend('<div id="nemid-terminal"></div><div id="nemid-terminal-scanlines"></div>');
+
+    $('.step1').waitUntilExists(function() {
+        console.log('Step one is ready...');
+    });
+
+    $('.Box-Content .otp').waitUntilExists(function() {
+        console.log('Step otp is ready...');
+        $(document).trigger('pin-step-ready');
+    });
     
+    // Init jQuery Terminal
     $('#nemid-terminal').terminal(function(rawCommand, term) {
         var command = rawCommand.split(' ')[0];
         var args = rawCommand.split(" ").slice(1);
@@ -19,30 +64,55 @@ jQuery(document).ready(function($) {
                 term.echo("No referrer found. ");
             }
         } else if (command == 'login') {
+            var $user = $('.input-2.person, #userid');
+            var $password = $('[type="password"]');
+            
             term.echo("Please enter your password");
-            var user = document.querySelector('#userid') || document.querySelector('.input-2.person');
-            user.value = args[0]; 
+            
+            $user.val(args[0]);
 
             console.log('Entered username: ' + args[0]);
 
-            //document.querySelector('#password').value = "";
+            term.push(function(command, term) {
+                $('.Box-Button-Submit').click();
 
-            term.push(function(password, term) {
-                if(command) {
-                	// '[type="password"]'
-                   document.querySelector('#password').value = password; 
+                if($('.error_txt').text() === "Fejl i bruger-id eller adgangskode.") {
+                    term.echo("Error logging in, please try again.");
+                    return false;
                 }
+
+                $(document).on('pin-step-ready', function() {
+                    console.log('Change terminal to PIN input');
+
+                    var cardNumber = $('.Keyheader').prev().text();
+                    var pinNeeded = $('#keyhelp').next().find('label').text();
+
+                    term.echo("Active keycard is " + cardNumber);
+                    term.echo("Please enter key for # " + pinNeeded);
+                    
+                    term.push(function(command, term) {
+                        $('.PIN').val(command);
+
+                        // Emulate click on the "logon button"
+                        $('.Box-Button-Submit').click();
+                    }, {
+                        prompt: 'PIN> ',
+                        name: 'pin'
+                    });
+                });
+
             }, {
                 prompt: '>',
                 name: 'password',
                 keydown: function(event, terminal) {
-                	console.log(event, terminal);
+                	console.log('Password: ', String.fromCharCode(event.keyCode));
                     if(event.keyCode !== 13) {
-                        document.querySelector('#password').value += 
+                        $password.val($password.val() + String.fromCharCode(event.keyCode));
                         return false;    
                     }
                 }
             });
+
         } else if (command == 'about') {
             term.echo("The Terminal Interface for NemID is brought to you by Allan Kimmer Jensen");
         } else if (command == 'shutdown') {
